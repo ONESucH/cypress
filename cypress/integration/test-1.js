119975ac4afb8de9d32e230d9f602f61cd21ad06
bogdanov <bogdanov@comepay.ru>
2019-09-17 10:41:08 +0300

    Проведены тесты, добавлены другие режимы тестов

--- cypress/integration/test-1.js
+++ cypress/integration/test-1.js
@@ -2,9 +2,36 @@ import formData from '../fixtures/form.json';
 
 const baseUrl = 'http://127.0.0.1:8080';
 
-describe('start test 1', function () {
-    it('load test 1', function () {
+describe('start test 1', () => {
+    it('load test 1', () => {
         cy.visit(baseUrl);
-        cy.get('#form').click()
-    })
+
+        cy.get('[name="name"]').focus().type(formData.name);
+        cy.get('[name="family"]').focus().type(formData.family);
+        cy.get('[name="male"]').check();
+        cy.get('[name="female"]').check();
+        cy.wait(5000);
+        cy.get('#form').submit();
+    });
+
+    it('load test 2', () => {
+        cy.get('[name="name"]').focus().type(formData.name);
+        cy.get('[name="family"]').focus().type(formData.family);
+        cy.get('[name="male"]').check();
+        cy.get('[name="female"]').check();
+        cy.get('#form').submit();
+        cy.wait(5000);
+    });
+
+    it('load test 3', () => {
+        cy.get('[name="name"]').focus().type(formData.name);
+        cy.wait(2000);
+        cy.get('[name="family"]').focus().type(formData.family);
+        cy.wait(2000);
+        cy.get('[name="male"]').check();
+        cy.wait(2000);
+        cy.get('[name="female"]').check();
+        cy.wait(2000);
+        cy.get('#form').submit();
+    });
 });
